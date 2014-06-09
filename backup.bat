c7bc3584fcb385050d74b4fff159bf9b24aff5e3
Alvin <alvinkwekel@gmail.com>
2014-06-09 23:20:00 +0200

    Added comments in code

--- backup.bat
+++ backup.bat
@@ -1,9 +1,12 @@
 @echo off
+REM Set title of the command window
 TITLE RoboBackup
 SETLOCAL DisableDelayedExpansion
 SET AppPath=%cd%
 SET SequenceFile=sequence.txt
+REM Hard coded customer ID used in the log file
 SET CustomerID=007
+REM Loop over entries in the sequence file
 FOR /F "usebackq delims=" %%a in (`"findstr /n ^^ %SequenceFile%"`) do (
     SET "BackupName=%%a"
     SETLOCAL EnableDelayedExpansion
@@ -12,5 +15,6 @@ FOR /F "usebackq delims=" %%a in (`"findstr /n ^^ %SequenceFile%"`) do (
     ENDLOCAL
 )
 ECHO All backups are completed.
+REM Shut down Windows
 ECHO Going to shutdown in 60 seconds.
 SHUTDOWN -s -t 60

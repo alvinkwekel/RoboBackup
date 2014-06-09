245997efa74e657d7b3dac7439e19179da34fbec
Alvin <alvinkwekel@gmail.com>
2014-06-09 22:28:55 +0200

    Handled log tailing with RoboCopy switch instead of tail.exe

--- runJob.bat
+++ runJob.bat
@@ -7,12 +7,10 @@ SET AppPath=%~f3
 REM Use UnxUtils date.exe to format current date-time
 FOR /f "tokens=*" %%i IN ('date.exe +"%%Y-%%m-%%dT%%H-%%M-%%S"') DO SET DateTime=%%i
 SET LogFile=%CustomerID%_%BackupName%_%DateTime%.log
-REM Use UnxUtils tail.exe to follow the log file
-START "RoboBackup log: %LogFile%" tail.exe -f --retry log\%LogFile%
 REM Output command as is going to be executed in same format as RoboCopy states its Job File and Log File
-ECHO  Command: ROBOCOPY /JOB:"%AppPath%\jobs\%BackupName%.RCJ" /LOG+:"%AppPath%\logs\%LogFile%" /FFT
+ECHO  Command: ROBOCOPY /JOB:"%AppPath%\jobs\%BackupName%.RCJ" /LOG+:"%AppPath%\log\%LogFile%" /FFT /TEE
 REM Actual command
-ROBOCOPY /JOB:"%AppPath%\jobs\%BackupName%.RCJ" /LOG+:"%AppPath%\log\%LogFile%" /FFT
+ROBOCOPY /JOB:"%AppPath%\jobs\%BackupName%.RCJ" /LOG+:"%AppPath%\log\%LogFile%" /FFT /TEE
 ECHO.
 ECHO Backup %BackupName% completed. Review log for details.
 ECHO.
